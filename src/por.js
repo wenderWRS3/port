bde644b423514c3390a0ba0b91d404ebd724a6f3
wenderWRS4 <wenderrodriguesdesouza675@gmail.com>
2025-12-25 20:05:18 -0300

    atul.  com menu burgue

---
+++ src/por.js
@@ -0,0 +1,13 @@
+var mmn = document.getElementById("desktopCllr")
+var ab1 = document.getElementById('bosta')
+function menu() {
+    mmn.style.display = "flex"
+    ab1.style.display = "none "
+    console.log(menu)
+}
+
+ function naoMenu(){
+    mmn.style.display = "none"
+    ab1.style.display = "flex"
+    console.log(naoMenu)
+ }
